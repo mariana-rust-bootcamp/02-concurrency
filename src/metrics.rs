3985aca11d90dee086ea0631067b2fbc7790f19c
Mariana <HealMSlin@gmail.com>
2025-02-26 19:40:17 +0800

    feat: support DashMap

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,38 +1,34 @@
-use core::fmt;
-use std::{
-    collections::HashMap,
-    fmt::Display,
-    sync::{Arc, RwLock},
-};
+use std::{collections::HashMap, fmt::Display, sync::Arc};
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
+use dashmap::DashMap;
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, i64>>>,
+    data: Arc<DashMap<String, i64>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Self {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
+        // 使用dashmap就不需要手动加锁lock()/write()了
         // 返回可变引用, 如果不存在则插入0再返回可变引用
-        let count = data.entry(key.into()).or_insert(0);
+        let mut count = self.data.entry(key.into()).or_insert(0);
         *count += 1;
         Ok(())
     }
-    // 快照
-    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        Ok(self
-            .data
-            .read()
-            .map_err(|e| anyhow!(e.to_string()))?
-            .clone())
+    // 快照 dashmap不允许直接获取内部的HashMap, 只能通过迭代器获取
+    pub fn snapshot(&self) -> HashMap<String, i64> {
+        let mut map = HashMap::new();
+        for entry in self.data.iter() {
+            map.insert(entry.key().to_string(), *entry.value());
+        }
+        map
     }
 }
 
@@ -45,10 +41,9 @@ impl Default for Metrics {
 impl Display for Metrics {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut res = String::new();
-        let data = self.data.read().map_err(|_e| fmt::Error)?;
         // RwLockReadGuard实现了Deref trait, 解引用时会返回内部的HashMap
-        for (k, v) in data.iter() {
-            res.push_str(&format!("{}: {}\n", k, v));
+        for entry in self.data.iter() {
+            res.push_str(&format!("{}: {}\n", entry.key(), entry.value()));
         }
         write!(f, "{}", res)
     }
